b89ec2ae5c6672afa31d07254f3b1c311f8e5173
Авиддарам Bazarragchaa <23LP3364@MacbookPros-MacBook-Pro-2.local>
2023-12-20 09:15:18 +0800

    test

--- assignments/easy/addElement/script.js
+++ assignments/easy/addElement/script.js
@@ -1,18 +1,26 @@
-let count=1;
-document.getElementById("root").innerHTML=`
+let count = 1;
+document.getElementById("root").innerHTML = `
 <div class="container flex">
 <div>
-<input type="text" id="text"> 
-<button class="flex"> Enter</button>
+<input type="text" id="num"> 
+<button class="flex" onclick="addAnyElement()">Enter</button>
 </div>
 <div id="boxCase" class="case flex"></div>
 <button onclick="addElement()">Add</button>
 </div>
 `;
-const addElement=()=>{
-    let box=document.createElement("div");
-    box.classList="box flex"
-    box.innerText=count;
-    document.getElementById("boxCase").appendChild(box);
-    count++;
+const addElement = () => {
+  let box = document.createElement("div");
+  box.classList = "box flex";
+  box.innerText = count;
+  document.getElementById("boxCase").appendChild(box);
+  count++;
+};
+const addAnyElement=()=>{
+    document.getElementById("boxCase").innerHTML=" ";
+    num=Number(document.getElementById("num").value);
+    for(let i=0; i<num; i++){
+        addElement();
+    }
+    count=1;
 }
---
+++ product/script.js
@@ -0,0 +1,183 @@
+const products = [
+  {
+    name: "Laptop",
+    price: 999.99,
+    brand: "ExampleBrand",
+    image_url:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-macbook-air-silver-m1-202010?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1634145618000",
+    description:
+      "Powerful laptop with high-performance specs for work and entertainment.",
+  },
+  {
+    name: "Smartphone",
+    price: 499.5,
+    brand: "TechCo",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "Cutting-edge smartphone with advanced features and a sleek design.",
+  },
+  {
+    name: "Headphones",
+    price: 99.0,
+    brand: "AudioTech",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "Comfortable headphones with excellent sound quality for immersive listening experiences.",
+  },
+  {
+    name: "Camera",
+    price: 799.0,
+    brand: "PhotoTech",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "Professional-grade camera capturing high-resolution photos and videos.",
+  },
+  {
+    name: "Tablet",
+    price: 299.99,
+    brand: "TabCo",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "Portable tablet perfect for entertainment and productivity on-the-go.",
+  },
+  {
+    name: "Smartwatch",
+    price: 199.5,
+    brand: "WearableTech",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "Smartwatch with health-tracking features and a stylish design.",
+  },
+  {
+    name: "Desktop Computer",
+    price: 1299.0,
+    brand: "ExampleBrand",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "High-performance desktop computer for demanding tasks and gaming.",
+  },
+  {
+    name: "Printer",
+    price: 149.0,
+    brand: "PrintTech",
+    image_url: "https://via.placeholder.com/300",
+    description: "Versatile printer suitable for both home and office use.",
+  },
+  {
+    name: "Keyboard",
+    price: 49.99,
+    brand: "InputTech",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "Ergonomic keyboard designed for comfortable and efficient typing.",
+  },
+  {
+    name: "Mouse",
+    price: 29.99,
+    brand: "InputTech",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "Responsive and precise mouse for seamless navigation and control.",
+  },
+  {
+    name: "Speaker System",
+    price: 399.0,
+    brand: "AudioTech",
+    image_url: "https://via.placeholder.com/300",
+    description: "Immersive speaker system delivering rich and clear audio.",
+  },
+  {
+    name: "External Hard Drive",
+    price: 129.0,
+    brand: "StorageTech",
+    image_url: "https://via.placeholder.com/300",
+    description: "High-capacity external hard drive for secure data storage.",
+  },
+  {
+    name: "Gaming Console",
+    price: 399.0,
+    brand: "GameTech",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "Next-gen gaming console offering stunning graphics and gaming experiences.",
+  },
+  {
+    name: "Router",
+    price: 79.99,
+    brand: "ConnectTech",
+    image_url: "https://via.placeholder.com/300",
+    description: "Reliable router providing high-speed internet connectivity.",
+  },
+  {
+    name: "Monitor",
+    price: 349.5,
+    brand: "DisplayTech",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "High-resolution monitor delivering vibrant and clear visuals.",
+  },
+  {
+    name: "Scanner",
+    price: 89.0,
+    brand: "ScanTech",
+    image_url: "https://via.placeholder.com/300",
+    description: "Efficient scanner for digitizing documents and images.",
+  },
+  {
+    name: "Fitness Tracker",
+    price: 79.0,
+    brand: "FitTech",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "Fitness tracker monitoring activity levels and health metrics.",
+  },
+  {
+    name: "Wireless Earbuds",
+    price: 129.0,
+    brand: "AudioTech",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "Wireless earbuds with premium audio quality and long battery life.",
+  },
+  {
+    name: "Security Camera",
+    price: 199.99,
+    brand: "SecureTech",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "High-definition security camera for home or business surveillance.",
+  },
+  {
+    name: "Electric Toothbrush",
+    price: 59.5,
+    brand: "DentalTech",
+    image_url: "https://via.placeholder.com/300",
+    description:
+      "Advanced electric toothbrush for thorough and effective dental care.",
+  },
+];
+let root = document.getElementById("root");
+const createCard = (prod) => {
+  let card = document.createElement("div");
+  card.className = "card";
+  let cardImage = document.createElement("img");
+  let imageLink = prod.image_url;
+  cardImage.src = imageLink;
+  card.appendChild(cardImage);
+  let title = document.createElement("h1");
+  title.innerText = prod.name;
+  card.appendChild(title);
+  let desc = document.createElement("p");
+  desc.innerText = prod.description;
+  card.appendChild(desc);
+  let price = document.createElement("h2");
+  price.innerText = prod.price + "$";
+  card.appendChild(price);
+  return card;
+};
+products.map((a) => {
+  root.appendChild(createCard(a));
+});
+const result=createCard(products[0]);
+console.log(result);
+  
